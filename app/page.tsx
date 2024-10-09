8cadf76a27506f86fe5522d4bac0953ceccccc5e
Peter Armitage <ficochu84@gmail.com>
2024-10-08 21:31:56 -0300

    fixed double render problem

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,14 @@
 'use client';
 import React, { useState, useRef, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import IntroSection from './components/IntroSection';
-import Home from './(routes)/Home/page';
-import Navigation from './components/Navigation/Navigation';
-import { Sections } from './types';
 
-const sections: Sections = {
-	home: { label: 'Home', component: <Home /> },
-	about: { label: 'About', component: null },
-	projects: { label: 'Projects', component: null },
-	skills: { label: 'Skills', component: null },
-	contacts: { label: 'Contacts', component: null },
-};
 const Page: React.FC = () => {
 	const [introStage, setIntroStage] = useState<
 		'video' | 'welcome' | 'complete'
 	>('video');
 	const videoRef = useRef<HTMLVideoElement>(null);
+	const router = useRouter();
 
 	useEffect(() => {
 		if (introStage === 'video') {
@@ -27,21 +19,18 @@ const Page: React.FC = () => {
 		}
 	}, [introStage]);
 
-	if (introStage !== 'complete') {
-		return (
-			<IntroSection
-				introStage={introStage}
-				videoRef={videoRef}
-				setIntroStage={setIntroStage}
-			/>
-		);
-	}
+	useEffect(() => {
+		if (introStage === 'complete') {
+			router.push('/Home');
+		}
+	}, [introStage, router]);
 
 	return (
-		<div className='relative w-full h-screen overflow-hidden'>
-			<Home />
-			<Navigation sections={sections} />
-		</div>
+		<IntroSection
+			introStage={introStage}
+			videoRef={videoRef}
+			setIntroStage={setIntroStage}
+		/>
 	);
 };
 
